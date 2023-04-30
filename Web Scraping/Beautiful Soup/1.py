284886eacef6b41abd3c13c6fee9e082b769d973
Teja Ambati <tejaambati20@gmail.com>
2023-04-30 23:41:28 +0530

    Beautiful Soup Files

--- Web Scraping/Beautiful Soup/1.py
+++ Web Scraping/Beautiful Soup/1.py
@@ -6,3 +6,15 @@ result = requests.get("www.google.com")
 content = result.text
 
 soup = BeautifulSoup(content,'lxml')
+
+# soup.find(id="specific_id")
+# soup.find('tag',class_="class_name")
+# find will returns the single element
+soup.find('article',class_="main_article")
+soup.find('h1')
+
+# To locate multiple elements by it's tag name, returns list
+# obtain multiple elements, all the elements will be stored in a list
+soup.find_all("h2")
+
+
