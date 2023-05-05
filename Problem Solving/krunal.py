e874d1075e988da305093965f480c1936d29aa80
Teja Ambati <tejaambati20@gmail.com>
2023-05-05 21:03:42 +0530

    another commit

---
+++ Problem Solving/krunal.py
@@ -0,0 +1,5 @@
+# Kunal is allowed to go out with his friends only on the even days of a given month. Write a program to count the number of days he can go out in the month of August.
+
+for i in range(1, 32):
+    if(i%2==0):
+        print(i)
